da5c817f7d5251d4c834b886b45d8990a87801ca
EmmaTheVibe <peteremmanuel316@gmail.com>
2026-05-14 09:37:37 +0100

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
   import "../app.css";
   import Navbar from "$lib/components/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import { inject } from "@vercel/analytics";
 
   const { children } = $props();
+
+  inject();
 </script>
 
 <Navbar />
